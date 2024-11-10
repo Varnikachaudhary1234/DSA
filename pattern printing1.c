2fe1abdf516bb11d8ab05811c37d34486cfb8811
varnikachaudhary1234 <varnikachaudhary2004@gmail.com>
2024-11-10 13:28:55 +0530

    pattern printing

---
+++ pattern printing1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k;
+    for (i = 1; i <= 4; i++) {
+        k = i;
+
+
+        for (j = 1; j <= 4 - i; j++) {
+            printf(" ");
+        }
+
+
+        for (j = 1; j <= i; j++, k++) {
+            printf("%d", k);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
+
+
